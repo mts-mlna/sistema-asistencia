5ebb77d45605db34174539d4f7f0c94f50af9689
Bruno Ezequiel Sánchez <3zequielbsanchez@gmail.com>
2025-08-28 17:08:37 -0300

    creado el esqueleto del sitio

--- app/views.py
+++ app/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.contrib import logout
+from django.db.models import Q
+from .models import *
 
 # Create your views here.
+def home(request):
+    return render(request, 'index.html')
